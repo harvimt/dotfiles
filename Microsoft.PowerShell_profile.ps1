e87d561818dafd554c550dd41b564192cbed62c3
Mark Harviston <infinull@gmail.com>
2025-11-24 12:32:46 -0800

    Make chocolatey tab complete work

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,12 @@
 oh-my-posh init pwsh --config ~/.omp.json | Invoke-Expression
 set-alias -Name g -Value git
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
